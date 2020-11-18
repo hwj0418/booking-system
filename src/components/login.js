87c3d80dcf6e4d52165880ae6db7c00297602527
Wj <ddh0418@gmail.com>
2020-11-17 21:09:14 -0500

    setting up structure

---
+++ src/components/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class login extends Component {
+  render() {
+    return (
+      <div> this is login </div>
+    );
+  }
+}
+
+export default login;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class home extends Component {
+  render() {
+    return (
+      <div> this is home page </div>
+    );
+  }
+}
+
+export default home;
